a6ff4f57ac335a384b0f85e03672d9eeaea0c345
kaypee3211 <kaypee3211@gmail.com>
2026-02-20 00:01:42 +0100

    good but not the best

--- colors.py
+++
@@ -1,9 +0,0 @@
-class color:
-    def BlackHeart(self):
-        self.CardColor = 'BlackHeart'
-    def RedClub(self):
-        self.CardColor = 'RedClub'
-    def BlackDiamond(self):
-        self.CardColor = 'BlackDiamond'
-    def RedSpade(self):
-        self.CardColor = 'RedSpade'
--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-from colors import color
-from CardClasses import classes
+import random
+
+class card:
+    def __init__(self,rank,color):
+        self.rank = rank
+        self.color = color
+    def __str__(self):
+        return f"{self.rank} {self.color}"
+
+rank = ['2','3','4','5','6','7','8','9','10', 'jack','queen', 'king', 'ace']
+colors = ['BlackHeart','RedClub','BlackDiamond','RedSpade']
+cards = []
+for i in rank:
+    for j in colors:
+        cards.append(card(i,j))
+print(cards[0]) #calling __str__ only of object of the class card and only when printing or converting on text
